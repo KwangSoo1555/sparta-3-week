9ca29b570752c012ca7927233804c42b1bcd6f1a
KwangSoo1555 <167167646+KwangSoo1555@users.noreply.github.com>
2024-05-02 19:08:07 +0900

    Add files via upload

---
+++ movie_page.js
@@ -0,0 +1,60 @@
+async function getdata() {
+    // localStorage에서 데이터 가져오기
+    const movieInfo = localStorage.getItem('movie-info');
+    const movieData = await JSON.parse(movieInfo);
+
+    return movieData;
+}
+console.log(getdata())
+
+function createSubPageCard(movieData, subPageCard) {
+    // 서브 페이지 카드 만들기
+    const subPageMovieDiv = `
+            <div class="col" id="movieCard">
+                <div class="card h-100">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w500${movieData.poster_path}" class="card-img-top" alt="이미지 준비중">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title" id="movieTitle">${movieData.title}</h5>
+                        <p class="card-text">${movieData.overview}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">★ ${movieData.vote_average}</small>
+                    </div>
+                </div>
+            </div>
+            `;
+    subPageCard.insertAdjacentHTML('beforebegin', subPageMovieDiv);
+}
+
+const print = async () => {
+    const data = await getdata();
+    const subPageCard = document.querySelector("#subpagecard");
+    
+    createSubPageCard(data, subPageCard);
+}
+
+print();
+
+function review_save() {
+    console.log("잘 됨");
+    console.log(document.querySelector("#review_name").value);
+
+    localStorage.setItem("review_name", document.querySelector("#review_name").value);
+    make_review_card();
+    window.location.reload();
+}
+
+function make_review_card() {
+    const review_div = `
+    <div class="card-body">
+        <h4 class="card-title">이름</h4>
+        <h6 class="card-subtitle mb-2 text-body-secondary">별점</h6>
+        <p class="card-text">리뷰 내용</p>
+        <a href="#" class="card-link">수정</a>
+        <a href="#" class="card-link">삭제</a>
+    </div>
+    `;
+    document.querySelector("#review_card").insertAdjacentHTML('beforeend', review_div);
+}
