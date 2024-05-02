c6cf54700d54ae1485159760d6387bdb2b216194
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-05-02 16:53:56 +0900

    main-05.02

---
+++ 05.02-merge-he/subPage.js
@@ -0,0 +1,37 @@
+async function getdata() {
+    // localStorage에서 데이터 가져오기
+    const movieInfo = localStorage.getItem('movie-info');
+    movieData = await JSON.parse(movieInfo);
+
+    return movieData;
+}
+
+function createSubPageCard(movieData, subPageCard) {
+    // 서브 페이지 카드 만들기
+    const subPageMovieDiv = `
+            <div class="col" id="movieCard">
+                <div class="card h-100">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w500${movieData.poster_path}" class="card-img-top" alt="이미지 준비중">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title" id="movieTitle">${movieData.title}</h5>
+                        <p class="card-text">${movieData.overview}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">★ ${movieData.vote_average}</small>
+                    </div>
+                </div>
+            </div>
+            `;
+    subPageCard.insertAdjacentHTML('beforeend', subPageMovieDiv);
+}
+
+const print = async () => {
+    const data = await getdata();
+    const subPageCard = document.querySelector("#subpagecard");
+    
+    createSubPageCard(data, subPageCard);
+}
+
+print();
