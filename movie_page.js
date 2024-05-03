7f81196a4a4fe8c4d3ec30543889656e5ad8336d
nalyn87 <nalyn0807@gmail.com>
2024-05-03 11:46:09 +0900

    리뷰 제작

--- movie_page.js
+++ movie_page.js
@@ -5,7 +5,6 @@ async function getdata() {
 
     return movieData;
 }
-console.log(getdata())
 
 function createSubPageCard(movieData, subPageCard) {
     // 서브 페이지 카드 만들기
@@ -28,33 +27,52 @@ function createSubPageCard(movieData, subPageCard) {
     subPageCard.insertAdjacentHTML('beforebegin', subPageMovieDiv);
 }
 
+// 리뷰 카드 생성
+function make_review_card() {
+    for (let i = 1; i <= localStorage.length; i++) {
+        const review = JSON.parse(localStorage.getItem(`review${i}`));
+
+        const review_div = `
+        <div class="review_card_body">
+            <h4 class="card-title">${review.review_name}</h4>
+            <h6 class="card-subtitle mb-2 text-body-secondary">${review.review_star}</h6>
+            <p class="card-text">${review.review_content}</p>
+            <a href="#" class="card-link">수정</a>
+            <a href="#" class="card-link">삭제</a>
+        </div>
+        `;
+
+        document.querySelector("#review_card").insertAdjacentHTML('beforeend', review_div);
+    }
+
+}
+
+// 최종 출력
 const print = async () => {
     const data = await getdata();
     const subPageCard = document.querySelector("#subpagecard");
     
     createSubPageCard(data, subPageCard);
+    make_review_card();
 }
 
 print();
 
+// 리뷰 값 저장
 function review_save() {
-    console.log("잘 됨");
     console.log(document.querySelector("#review_name").value);
+    
+    const review = {};
+    review.review_name = document.querySelector("#review_name").value;
+    review.review_star = document.querySelector("#review_star").value;
+    review.review_content = document.querySelector("#review_content").value;
+    review.review_pw = document.querySelector("#review_pw").value;
+    
 
-    localStorage.setItem("review_name", document.querySelector("#review_name").value);
-    make_review_card();
+    console.log(review);
+
+    localStorage.setItem(`review${localStorage.length}`, JSON.stringify(review));
+
+    alert("리뷰가 작성되었습니다!");
     window.location.reload();
-}
-
-function make_review_card() {
-    const review_div = `
-    <div class="card-body">
-        <h4 class="card-title">이름</h4>
-        <h6 class="card-subtitle mb-2 text-body-secondary">별점</h6>
-        <p class="card-text">리뷰 내용</p>
-        <a href="#" class="card-link">수정</a>
-        <a href="#" class="card-link">삭제</a>
-    </div>
-    `;
-    document.querySelector("#review_card").insertAdjacentHTML('beforeend', review_div);
 }
